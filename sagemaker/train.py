70d8a65a6e09306fe23d01f2efbda3f147cd5c80
IshikaSaxena <ishika.3.saxena@bt.com>
2026-01-30 21:00:23 +0530

    adding train.py

--- sagemaker/train.py
+++ sagemaker/train.py
@@ -3,6 +3,9 @@ import joblib
 import os
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
 
 # SageMaker paths
 DATA_DIR = "/opt/ml/input/data/train"
@@ -19,10 +22,27 @@ def main():
     df['Churn'] = df['Churn'].map({'Yes': 1, 'No': 0})
     df.drop('customerID', axis=1, inplace=True)
 
-    df = pd.get_dummies(df, drop_first=True)
-
+    # df = pd.get_dummies(df, drop_first=True)
     X = df.drop('Churn', axis=1)
     y = df['Churn']
+
+    categorical_cols = X.select_dtypes(include=['object']).columns
+    numericla_cols= X.select_dtypes(exclude=['object']).columns
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('cat',OneHotEncoder(handle_unknown='ignore'),categorical_cols),
+            ('num','passthrough',numericla_cols)
+        ]
+    )
+
+    model = Pipeline(steps=[
+        ('preprocessor',preprocessor),
+        ('classifier',RandomForestClassifier(
+            n_estimators=200,
+            random_state=42
+        ))
+    ])
 
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42
